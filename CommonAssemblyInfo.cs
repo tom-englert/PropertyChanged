1a78a9e43f057825b97bd17956199a8e964b5925
tom-englert <mail@tom-englert.de>
2018-01-16 12:22:49 +0100

    V 2.1.4.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("PropertyChanged2")]
 [assembly: AssemblyCompany("Tom Englert, Simon Cropp and Contributors")]
 [assembly: AssemblyDescription("Fody add-in for injecting INotifyPropertyChanged code into properties.")]
-[assembly: AssemblyVersion("2.1.4.2")]
-[assembly: AssemblyFileVersion("2.1.4.2")]
+[assembly: AssemblyVersion("2.1.4.3")]
+[assembly: AssemblyFileVersion("2.1.4.3")]
