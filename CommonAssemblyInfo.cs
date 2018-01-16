222c6e89a5009c2e91d73c49e5ef0961875201a2
tom-englert <mail@tom-englert.de>
2018-01-16 11:08:40 +0100

    V 2.1.4.2

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("PropertyChanged")]
-[assembly: AssemblyProduct("PropertyChanged")]
-[assembly: AssemblyCompany("Simon Cropp and Contributors")]
+[assembly: AssemblyTitle("PropertyChanged2")]
+[assembly: AssemblyProduct("PropertyChanged2")]
+[assembly: AssemblyCompany("Tom Englert, Simon Cropp and Contributors")]
 [assembly: AssemblyDescription("Fody add-in for injecting INotifyPropertyChanged code into properties.")]
-[assembly: AssemblyVersion("2.1.4.1")]
-[assembly: AssemblyFileVersion("2.1.4.1")]
+[assembly: AssemblyVersion("2.1.4.2")]
+[assembly: AssemblyFileVersion("2.1.4.2")]
